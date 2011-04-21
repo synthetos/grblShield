44145308b611fbb8403f49cdca2b068b8e7edde7
ril3y <rileyporter@gmail.com>
2011-04-21 17:51:20 -0400

    Initial Commit

---
+++ default/dep/motion_control.o.d
@@ -0,0 +1,68 @@
+motion_control.o: ../motion_control.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../settings.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/math.h ../motion_control.h \
+  ../planner.h c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  ../nuts_bolts.h c:/winavr-20100110/lib/gcc/../../avr/include/string.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdbool.h \
+  ../stepper.h c:/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h \
+  ../wiring_serial.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../settings.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/math.h:
+
+../motion_control.h:
+
+../planner.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+../nuts_bolts.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdbool.h:
+
+../stepper.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h:
+
+../wiring_serial.h:
---
+++ nuts_bolts.h
@@ -0,0 +1,42 @@
+/*
+  motion_control.h - cartesian robot controller.
+  Part of Grbl
+
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef nuts_bolts_h
+#define nuts_bolts_h
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define false 0
+#define true 1
+
+#define X_AXIS 0
+#define Y_AXIS 1
+#define Z_AXIS 2
+
+#define clear_vector(a) memset(a, 0, sizeof(a))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
+// Read a floating point value from a string. Line points to the input buffer, char_counter 
+// is the indexer pointing to the current character of the line, while double_ptr is 
+// a pointer to the result variable. Returns true when it succeeds
+int read_double(char *line, uint8_t *char_counter, double *double_ptr);
+
+#endif
